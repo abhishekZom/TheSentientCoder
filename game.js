10bd76c4a1e267e8e7f046154f21f4df5749a5a6
Abhishek Kumar <abhi28k09@gmail.com>
2023-06-24 15:56:59 +0530

    representations

--- game.js
+++ game.js
@@ -1,4 +1,12 @@
 // empty spaces are represented by a "."
+// walls are represented by "#"
+// lava is represented by "+"
+// the player's initial position is "@"
+// the coins are represented by "O"
+// the horizonatally moving lava is "="
+// the dripping lava is represented by "v"
+// the vertically moving blob is "|"
+
 
 
 let simpleLevelPlan = `
